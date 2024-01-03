e10d7c441fb4a9a5a1a126fc9a7d810144171054
PaulTrusso <pault1433@gmail.com>
2024-01-03 15:45:41 -0600

    secondarycommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-
-
+import { puppyList } from './data.js'
+import {useState} from 'react'
 import './App.css'
 
+
+
 function App() {
-
-
+  const [featPupId, setFeatPupId] = useState(null)
+  const [puppies, setPuppies] = useState(puppyList)
+console.log("puppyList: ", puppyList);
+const featuredPup = puppies.find((pup)=>pup.id === featPupId)
+console.log(featuredPup)
   return (
     <>
-      <div>
-        <p> hello world</p>
+   
+      <div className='pupList'>
+       {
+        puppies.map((puppy) => {
+        return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        })
+       }
+
+{featPupId && (
+        <div className='singlePup'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
+
       </div>
     
     </>
